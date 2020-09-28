36061694878700d0830860f6ef5d515782507811
Sam Joy <samjoy.productions@gmail.com>
2020-09-28 14:12:53 -0400

    Updates

---
+++ precache-manifest.f0395d7e5628ee23d2e7b7a448bc5164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b22f93a878132bf569cae1d61588a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb786dbdb05d283c3f41",
+    "url": "/static/css/main.bfb3b11d.chunk.css"
+  },
+  {
+    "revision": "a3ab2794d704e0c69e98",
+    "url": "/static/js/2.c2992d1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c2992d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb786dbdb05d283c3f41",
+    "url": "/static/js/main.1d6c54e9.chunk.js"
+  },
+  {
+    "revision": "7f952841af7c14ad4c9b",
+    "url": "/static/js/runtime-main.b101e3d6.js"
+  },
+  {
+    "revision": "957a8228d680e7a858a97e3e08c313f8",
+    "url": "/static/media/nightcrawler.957a8228.jpg"
+  }
+]);
